984a827e937d260e685c8520263a178463e5f02c
Sergy Stepanov <sergystepanov@gmail.com>
2016-10-15 23:06:04 +0300

    Add settings

---
+++ src/main/java/com/mvzic/extra/app/AppSettings.java
@@ -0,0 +1,30 @@
+package com.mvzic.extra.app;
+
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
+
+public class AppSettings {
+    private static final String settingsRootName = "mvzic-dropbox-java";
+    private final Preferences preferences;
+
+    public AppSettings() {
+        preferences = Preferences.userRoot().node(settingsRootName);
+
+        try {
+            String[] keys = preferences.keys();
+            for (String key : keys) {
+                System.out.println(key);
+            }
+        } catch (BackingStoreException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void set(final String key, final String value) {
+        preferences.put(key, value);
+    }
+
+    public String get(final String key) {
+        return preferences.get(key, "");
+    }
+}
---
+++ src/main/java/com/mvzic/extra/app/Settings.java
@@ -0,0 +1,5 @@
+package com.mvzic.extra.app;
+
+public interface Settings {
+    String TOKEN = "token";
+}
