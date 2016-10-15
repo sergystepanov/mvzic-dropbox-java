f127b88da383354710f800399b0845766c0fd37a
Sergy Stepanov <sergystepanov@gmail.com>
2016-10-15 23:06:17 +0300

    Add bar

---
+++ src/main/java/com/mvzic/extra/ui/Bar.java
@@ -0,0 +1,10 @@
+package com.mvzic.extra.ui;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
+public class Bar extends Pane {
+    public Bar() {
+        this.getChildren().add(new Label("Bar"));
+    }
+}
